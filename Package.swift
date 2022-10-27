b2ffbdb446adadac03bfba04f20f762ade0af548
Marko Benacic <34174145+Markobenacic@users.noreply.github.com>
2022-10-27 13:16:28 +0200

    package info changes

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "iOS-sdk",
-            targets: ["CorvusWalletSDK"]),
+            targets: ["iOS-sdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
