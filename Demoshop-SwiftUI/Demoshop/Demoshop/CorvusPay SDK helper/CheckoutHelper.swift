774c938450763d2e463f892b021b06d4b25e52d8
Marko Benacic <benacicmarko@gmail.com>
2025-02-27 12:54:16 +0100

    checkout helper

---
+++ Demoshop-SwiftUI/Demoshop/Demoshop/CorvusPay SDK helper/CheckoutHelper.swift
@@ -0,0 +1,15 @@
+//
+//  CheckoutHelper.swift
+//  Demoshop
+//
+//  Created by Marko Benačić on 27.02.2025..
+//
+
+import Foundation
+import CorvusWalletSDK
+
+class CheckoutHelper {
+    func createCheckout(with installmentType: InstallmentType) -> Checkout {
+        
+    }
+}
